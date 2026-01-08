16b007901a063f6ff5cbe8b3f98c271bddb8e0a4
Varun T <varunvarun60954@gmail.com>
2026-01-08 20:26:18 +0530

    Binary search completed

--- src/Binary_Searching/BinarySearch.java
+++ src/Binary_Searching/BinarySearch.java
@@ -2,6 +2,30 @@ package Binary_Searching;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = -12;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
 
+    static int binarySearch(int[] arr, int element){
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start+(end-start)/2;
+
+            if(element< arr[mid]){
+                end = mid-1;
+            } else if (element>arr[mid]) {
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
     }
 }
